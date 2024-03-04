e13441a8e45d6a7ad71f243fe4aba8ea5a90d934
jguegen <al.juan.guerra.gennich@iesportada.com>
2024-03-04 20:10:18 +0100

    Testing v1

---
+++ app/src/test/java/com/juanlugg8/fitnessmanager/UserTest.kt
@@ -0,0 +1,13 @@
+package com.juanlugg8.fitnessmanager
+
+import com.google.common.truth.Truth
+import com.juanlugg8.fitnessmanager.entity.User
+import org.junit.Test
+
+class UserTest {
+    val user = User(1,"Juanlu","666777888")
+    @Test
+    fun `user is Instance of User`(){
+        Truth.assertThat(user).isInstanceOf(User::class.java)
+    }
+}
