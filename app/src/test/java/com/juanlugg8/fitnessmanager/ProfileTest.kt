438af415166511c2a111b53152455a5abe1acd3c
juanlugg8 <juanlugg18@gmail.com>
2024-03-04 20:51:46 +0100

    Testing v3

--- app/src/test/java/com/juanlugg8/fitnessmanager/ProfileTest.kt
+++ app/src/test/java/com/juanlugg8/fitnessmanager/ProfileTest.kt
@@ -7,19 +7,32 @@ import com.juanlugg8.fitnessmanager.repository.UserRepository
 import org.junit.Test
 
 class ProfileTest {
-    val user = User(1,"Juanlu","666777888")
-    val profile = Profile(1, user, "04/03/2024",180.0,80.0)
+    val user = User(1, "Juanlu", "666777888")
+    val profile = Profile(1, user, "04/03/2024", 180.0, 80.0)
+
     @Test
-    fun `profile is Instance of Profile`(){
+    fun `profile is Instance of Profile`() {
         Truth.assertThat(profile).isInstanceOf(Profile::class.java)
     }
+
     @Test
-    fun `profileId is equals 1`(){
+    fun `profileId is equals 1`() {
         Truth.assertThat(profile.id).isEqualTo(1)
     }
+
     @Test
-    fun `user is correct`(){
-        val user = User(1,"Juanlu","666777888")
+    fun `profile create`() {
+        val id = 1
+        val user = user
+        val date = "04/03/2024"
+        val height = 180.0
+        val weight = 80.0
+        Truth.assertThat(profile).isEqualTo(Profile(id,user,date,height,weight))
+    }
+
+    @Test
+    fun `user is correct`() {
+        val user = User(1, "Juanlu", "666777888")
         Truth.assertThat(profile.user).isEqualTo(user)
     }
 }
--- app/src/test/java/com/juanlugg8/fitnessmanager/UserTest.kt
+++ app/src/test/java/com/juanlugg8/fitnessmanager/UserTest.kt
@@ -1,6 +1,7 @@
 package com.juanlugg8.fitnessmanager
 
 import com.google.common.truth.Truth
+import com.juanlugg8.fitnessmanager.entity.Profile
 import com.juanlugg8.fitnessmanager.entity.User
 import org.junit.Test
 
@@ -9,6 +10,13 @@ class UserTest {
     @Test
     fun `user is Instance of User`(){
         Truth.assertThat(user).isInstanceOf(User::class.java)
+    }
+    @Test
+    fun `user create`() {
+        val id = 1
+        val name = "Juanlu"
+        val phone = "666777888"
+        Truth.assertThat(user).isEqualTo(User(id,name,phone))
     }
     @Test
     fun `userId is equals 1`(){
